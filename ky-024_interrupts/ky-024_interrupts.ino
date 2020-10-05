63f8b6b8435dbe04991c98b7e44d3bbfe417fbf9
David Poza Suárez <pozasuarez@gmail.com>
2020-10-05 19:07:51 +0200

    calculations using measured time for loop

--- ky-024_interrupts/ky-024_interrupts.ino
+++ ky-024_interrupts/ky-024_interrupts.ino
@@ -24,31 +24,42 @@
 
 int digitalPin = 3;
 int rpm;
-int measurements[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+float measurements[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 int mIndex = 0; // current measurement index in array
 volatile int ticks;
+unsigned long time;
 
-float transformacionLineal(int ticks){
-  if(ticks>0)
-    return(0.469*ticks+1.71); // de momento me la invento
-  else
-    return(0);
-}
-
-void setup() {
-  rpm = 0;
-  ticks = 0;
-  attachInterrupt(digitalPinToInterrupt(digitalPin), acumulador, FALLING);
-  Serial.begin(9600);
-
-}
-
-float averageWindSpeed(int measurements[10]) {
-  int acc = 0;
+/**
+ * Calculates average wind speed using last ten measurements
+ */
+float averageWindSpeed(float measurements[10]) {
+  float acc = 0;
   for(int i=0; i<10; i++) {
     acc += measurements[i];
   }
   return acc / 10;
+}
+
+/**
+ * Transforms ticks per second to Km/h
+ */
+float windLinearTransformation(float ticksSec){
+  Serial.print("Ticks per sec = ");
+  Serial.println(ticksSec);
+  if(ticksSec>0)
+    return(1.41*ticksSec+1.71);
+  else
+    return(0);
+}
+
+
+void setup() {
+  rpm = 0;
+  ticks = 0;
+  time = millis();
+  attachInterrupt(digitalPinToInterrupt(digitalPin), acumulador, FALLING);
+  Serial.begin(9600);
+
 }
 
 void loop() {
@@ -58,20 +69,19 @@ void loop() {
   //------- no queremos que se interrumpa el cálculo
   
   rpm = ticks*1000*60 / (3000*3);
-  measurements[mIndex] = transformacionLineal(ticks);
-  mIndex++;
-  if (mIndex == 10) {
-    mIndex = 0;
-  }
+  measurements[mIndex] = windLinearTransformation(ticks * 1000 / (millis() - time));
+  
+  
   Serial.print("current wind speed km/h: ");
-  Serial.println(transformacionLineal(ticks));
+  Serial.println(measurements[mIndex]);
   Serial.print("rpm: ");
   Serial.println(rpm);
   Serial.print("ticks por cycle: ");
   Serial.println(ticks);
   Serial.print("average wind speed km/h: ");
   Serial.println(averageWindSpeed(measurements));
-
+  time = millis();
+  mIndex = (mIndex + 1) % 10;
   ticks = 0;
   //-------
   attachInterrupt(digitalPinToInterrupt(digitalPin), acumulador, FALLING);
